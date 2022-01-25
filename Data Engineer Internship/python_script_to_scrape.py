4cc5db51c3937418350f1311e6eb90c74e5f09af
shoayebhave <70103164+shoayebhave@users.noreply.github.com>
2022-01-25 10:24:57 +0600

    Add files via upload

---
+++ Data Engineer Internship/python_script_to_scrape.py
@@ -0,0 +1,41 @@
+"""import"""
+import pandas as pd
+import numpy as np
+import requests
+import json
+import time
+final_data = []
+# Parameters
+coordinates = ['23.777176,90.399452']
+keywords = ['all_restaurants_in_bangladesh']
+radius = '1000'
+api_key = 'AIzaSyD1qkItnF9DfuqEyaA6NIyfB11W2G2udyI'  # insert your Places API
+for coordinate in coordinates:
+    for keyword in keywords:
+        url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + coordinate + '&radius=' + str(
+            radius) + '&keyword=' + str(keyword) + '&key=' + str(api_key)
+        while True:
+            print(url)
+            respon = requests.get(url)
+            jj = json.loads(respon.text)
+            results = jj['results']
+
+            for result in results:
+                name = result['name']
+                lat = result['geometry']['location']['lat']
+                lng = result['geometry']['location']['lng']
+                rating = result['rating']
+                number_of_reviews = result['user_ratings_total']
+
+                data = [name, lat, lng, rating, number_of_reviews]
+                final_data.append(data)
+                time.sleep(5)
+            if 'next_page_token' not in jj:
+                break
+            else:
+                next_page_token = jj['next_page_token']
+                url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=' + str(
+                    api_key) + '&pagetoken=' + str(next_page_token)
+                labels = ['Place Name', 'Latitude', 'Longitude', 'Rating', 'Number_Of_Reviews']
+                export_dataframe_1_medium = pd.DataFrame.from_records(final_data, columns=labels)
+                export_dataframe_1_medium.to_csv('DE_Assignment_Internship_ Quarter_One_MD_Shoayeb_Islam.csv')
